73edb25ab2e4f648af4a6a96279bca427b2ecdc2
garfilxploit <sycoxcore@gmail.com>
2022-08-15 15:26:19 +0800

    botvir

---
+++ menu/help.js
@@ -0,0 +1,31 @@
+const help = (prefix,shp,monoscape) => {
+	return `╭─「 *MENU TAMBAHAN*  」
+ |
+ ◦➛ .script
+ ◦➛ .setprefix multi/nopref
+ ◦➛ .virgam
+ ◦➛ .public
+ ◦➛ .self
+ |
+╰─「 *MENU TAMBAHAN* 」
+
+╭─「 *MENU CONVERT VIRUS GAMBAR*  」
+ |
+ ◦➛ .addvir
+ ◦➛ .getvir
+ ◦➛ .listvir
+ |
+╰─「 *MENU CONVERT VIRUS GAMBAR* 」
+
+╭─「 *CONVERT VIRUS VIDEO*  」
+ |
+ ◦➛ .addvrv
+ ◦➛ .getvrv
+ ◦➛ .listvrv 
+ |
+╰─「 *CONVERT VIRUS VIDEO* 」
+
+`
+}
+
+exports.help = help
